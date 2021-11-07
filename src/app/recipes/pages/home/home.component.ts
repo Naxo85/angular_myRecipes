e8455742d25f7b14b156eabe3d5db69c92dbfa3f
ianeiros <naxo85@gmail.com>
2021-11-07 11:03:46 +0100

    initial module/component creations

---
+++ src/app/recipes/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-recipes',
+  templateUrl: './saved-recipes.component.html',
+  styles: [],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
+import { DetailsComponent } from './pages/details/details.component';
+
+@NgModule({
+  declarations: [HomeComponent, CreateComponent, DetailsComponent],
+  imports: [CommonModule],
+})
+export class RecipesModule {}
