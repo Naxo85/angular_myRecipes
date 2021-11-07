f8d81743d03aa4b590ffddea75f509011c935f4b
ianeiros <naxo85@gmail.com>
2021-11-07 11:56:48 +0100

    main routes and error page component

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomeComponent } from './pages/home/home.component';
 import { CreateComponent } from './pages/create/create.component';
 import { DetailsComponent } from './pages/details/details.component';
