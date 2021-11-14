0ac1f5a4092de5318d711d2d719e71810fd20d99
ianeiros <naxo85@gmail.com>
2021-11-14 11:50:11 +0100

    Navbar first version

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './pages/create/create.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 
@@ -8,6 +9,10 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
     children: [
+      {
+        path: 'create',
+        component: CreateComponent,
+      },
       {
         path: 'list',
         component: ListComponent,
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { HomeComponent } from './pages/home/home.component';
 import { CreateComponent } from './pages/create/create.component';
@@ -10,6 +12,6 @@ import { ListComponent } from './pages/list/list.component';
 
 @NgModule({
   declarations: [HomeComponent, CreateComponent, DetailsComponent, ListComponent],
-  imports: [CommonModule, RecipesRoutingModule],
+  imports: [CommonModule, RecipesRoutingModule, FlexLayoutModule, MaterialModule],
 })
 export class RecipesModule {}
